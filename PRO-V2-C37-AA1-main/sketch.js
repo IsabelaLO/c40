86a6a1cd7280fc3d469267c3c176268de349d32c
IsabelaLO <98844370+IsabelaLO@users.noreply.github.com>
2022-06-15 09:37:46 -0300

    Add files via upload

---
+++ PRO-V2-C37-AA1-main/sketch.js
@@ -0,0 +1,40 @@
+var canvas;
+var backgroundImage, bgImg, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2;
+var cars = [];
+var moedas, moedaImg;
+var combustivel, combustivelImg
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/track.jpg");
+  moedaImg = loadImage("../assets/goldCoin.png");
+  combustivelImg = loadImage("../assets/fuel.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
